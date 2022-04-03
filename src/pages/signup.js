4619dc47e941e6aa41b70ac12e8ed1ef210bebc1
BLADIMIR ROJAS <bladimirrojassepulveda@gmail.com>
2022-04-02 19:09:11 -0500

    signUp

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet, ImageBackground} from "react-native";
+import { View, Text, ScrollView, StyleSheet, TextInput, Button } from "react-native";
 
-export default function SignUp(){
+export default function SignUp({ navigation }){
     return (
      
       
         <ScrollView style={styles.SignUp}>
          <View>
         <View >
-        <Text style={styles.tittle}> 
-            page under development</Text>
+        <Text style={styles.tittle}>Create your MyTinerary account !!t</Text>
+        <TextInput  style={styles.Input}  placeholder="You First Name" />
+        <TextInput  style={styles.Input}  placeholder="You First Lastname" />
+        <TextInput  style={styles.Input}  placeholder="You email" />
+        <TextInput  style={styles.Input}  placeholder="Password" />
+        <TextInput  style={styles.Input}  placeholder="Avatar" />
+        <View style={styles.Button}>
+        <Button  
+        onPress={() => navigation.navigate('Home')}
+        title="SignUp"
+        color="#75777A"
+      
+      />
+      </View>
         </View>
         </View>   
         
@@ -23,15 +35,40 @@ const styles = StyleSheet.create({
     SignUp:{
     display: "flex",
     alignContent: "center",
-    height: 100,
+    height: 90,
     width: "100%",
-    backgroundColor:"#5D5B5A",
+    backgroundColor:"#7983E7",
   },
   tittle:{
-    fontSize: 40,
+    fontSize: 25,
     color: "#F8F3F2",
     textAlign: "center",
     paddingTop: 10,
+    marginTop:25,
+    marginBottom: 30,
  },
+ Input: {
+  width: 340,
+  height:45,
+  marginTop: 5,
+  marginLeft:10,
+  borderRadius: 20,
+  backgroundColor:"white",
+  textAlign:"center",
+  marginBottom: 15,
+},
+Button: {
+  width: "100%",
+  height: 60,
+  paddingLeft:20,
+  paddingRight:20,
+  marginTop: 15,
+  fontSize: 12,
+  
+},
+
+
+
+
  
 });

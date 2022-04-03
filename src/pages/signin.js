b85db5ad504150d01740ea7291b3f7fe065362de
BLADIMIR ROJAS <bladimirrojassepulveda@gmail.com>
2022-04-02 19:26:52 -0500

    add signIn

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet, ImageBackground} from "react-native";
+import { View, Text, ScrollView, StyleSheet, TextInput, Button } from "react-native";
 
 export default function SignIn(){
     return (
      
       
-        <ScrollView style={styles.SignIn}>
-         <View>
-        <View >
-        <Text style={styles.tittle}> 
-            page under development</Text>
-        </View>
-        </View>   
-        
-        </ScrollView>
+      <ScrollView style={styles.SignIn}>
+      <View>
+     <View >
+     <Text style={styles.tittle}>Wellcome!! your MyTinerary</Text>
+     <TextInput  style={styles.Input}  placeholder="You email" />
+     <TextInput  style={styles.Input}  placeholder="Password" />
+     <View style={styles.Button}>
+     <Button  
+     onPress={() => navigation.navigate('Home')}
+     title="SignUp"
+     color="#75777A"
+   
+   />
+   </View>
+     </View>
+     </View>   
+     
+     </ScrollView>
    
   );
 };
@@ -25,13 +34,34 @@ const styles = StyleSheet.create({
     alignContent: "center",
     height: 100,
     width: "100%",
-    backgroundColor:"#5D5B5A",
+    backgroundColor:"#7983E7",
   },
   tittle:{
-    fontSize: 40,
+    fontSize: 24,
     color: "#F8F3F2",
     textAlign: "center",
-    paddingTop: 10,
+    paddingTop: 20,
+    marginTop:50,
+    marginBottom: 30,
  },
- 
+ Input: {
+  width: 340,
+  height:45,
+  marginTop: 10,
+  marginLeft:10,
+  borderRadius: 20,
+  backgroundColor:"white",
+  textAlign:"center",
+  marginBottom: 15,
+},
+Button: {
+  width: "100%",
+  height: 60,
+  paddingLeft:20,
+  paddingRight:20,
+  marginTop: 15,
+  fontSize: 12,
+  
+},
+
 });
